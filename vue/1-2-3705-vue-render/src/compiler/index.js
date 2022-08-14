0b8214f5b23bff63ad435e0722c0f8586555238b
staticdeng <1301829682@qq.com>
2022-08-14 23:05:02 +0800

    vue原理：编译模版转为 ast 语法树

--- vue/1-2-3705-vue-render/src/compiler/index.js
+++ vue/1-2-3705-vue-render/src/compiler/index.js
@@ -1,7 +1,13 @@
 /**
- * 编译器
+ * https://github.com/vuejs/vue/blob/2.6/src/compiler/index.js
+ * 编译器：
+    • 转换 HTML 为 AST
+
  */
+import { parse } from './parser/index';
 
 export function compileToFunctions(template) {
-  console.log(template);
+  // 1. 转换 template 为 ast 语法树
+  const ast = parse(template.trim());
+  console.log(ast);
 }
